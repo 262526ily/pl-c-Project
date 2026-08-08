1284a1dd272436583d2b27ab156f87aa18c601a5
Bo-Gong <3614204641@qq.com>
2026-08-08 17:44:06 +0800

    内联乘法优化

--- test.s
+++ test.s
@@ -1,6 +1,7 @@
     .text
 
     .text
+<<<<<<< HEAD
     .globl mod_large
 mod_large:
     addi sp, sp, -16
@@ -18,6 +19,26 @@ mod_large:
     lw ra, -4(fp)
     lw fp, -8(fp)
     addi sp, sp, 16
+=======
+    .globl mul
+mul:
+    addi sp, sp, -32
+    sw ra, 28(sp)
+    sw fp, 24(sp)
+    addi fp, sp, 32
+    sw a0, -12(fp)
+    sw a1, -16(fp)
+    lw t0, -12(fp)
+    lw t1, -16(fp)
+    mul t0, t0, t1
+    sw t0, -20(fp)
+    lw a0, -20(fp)
+    j .L_epilogue_mul
+.L_epilogue_mul:
+    lw ra, -4(fp)
+    lw fp, -8(fp)
+    addi sp, sp, 32
+>>>>>>> 3d3e7f4 (内联乘法优化)
     ret
 
     .text
@@ -27,12 +48,24 @@ main:
     sw ra, 28(sp)
     sw fp, 24(sp)
     addi fp, sp, 32
+<<<<<<< HEAD
     li t0, 5000
     sw t0, -16(fp)
     lw a0, -16(fp)
     call mod_large
     sw a0, -20(fp)
     lw t0, -20(fp)
+=======
+    li t0, 10
+    sw t0, -20(fp)
+    li t0, 20
+    sw t0, -16(fp)
+    lw a0, -20(fp)
+    lw a1, -16(fp)
+    call mul
+    sw a0, -24(fp)
+    lw t0, -24(fp)
+>>>>>>> 3d3e7f4 (内联乘法优化)
     sw t0, -12(fp)
     lw a0, -12(fp)
     j .L_epilogue_main
--- test/test.c
+++ test/test.c
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 // test_mod_large.tc
 int mod_large(int x) {
     return x % 4096;
@@ -7,5 +8,16 @@ int mod_large(int x) {
 int main() {
     int x = 5000;
     int z = mod_large(x);
+=======
+// test_runtime_mul.tc
+int mul(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    int x = 10;
+    int y = 20;
+    int z = mul(x, y);
+>>>>>>> 3d3e7f4 (内联乘法优化)
     return z;
 }
