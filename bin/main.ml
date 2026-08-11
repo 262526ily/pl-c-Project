450da157d82c34dbbc2cc98d0d1dadf7362f62a4
Bo-Gong <3614204641@qq.com>
2026-08-11 12:43:47 +0800

    寄存器分配

--- bin/main.ml
+++ bin/main.ml
@@ -37,8 +37,8 @@ let () =
         
         (* 【修改】注释掉原本的 IR 打印和成功提示 *)
         
-        Printf.printf "Semantic check success!\n";
-        Lib.Ir.dump_ir ir ;
+        (* Printf.printf "Semantic check success!\n";
+        Lib.Ir.dump_ir ir ; *)
         (* 【关键】直接调用汇编代码生成器，将 RV32I 汇编流打印到标准输出 *)
         Lib.Codegen.generate_riscv optimized_ir
     );
