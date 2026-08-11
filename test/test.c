b8ac14a625d388a92a8b5bf70908dd08aea1fe95
Bo-Gong <3614204641@qq.com>
2026-08-11 16:03:39 +0800

    licm优化回退

--- test/test.c
+++ test/test.c
@@ -1,18 +1,19 @@
-// test_licm_2.tc
-const int A = 10;
-const int B = 20;
-const int C = 30;
-const int N = 50;
+// test_licm_3.tc
+const int N = 20;
+const int A = 5;
 
 int main() {
     int sum = 0;
     int i = 0;
-    int step = A + B;        // 30（不变量）
-    int limit = N + C;       // 80（不变量）
+    int step = A * 2;        // 10（不变量）
     
-    while (i < limit) {
-        sum = sum + step;
+    while (i < N) {
+        if (i > 10) {
+            sum = sum + step * 2;   // step 不变
+        } else {
+            sum = sum + step;       // step 不变
+        }
         i = i + 1;
     }
-    return sum;              // 80 * 30 = 2400
+    return sum;              // 10*10 + 10*20 = 300
 }
