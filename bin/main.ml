85be3c2da6d4d0fc9399661cd2268625b2ce8540
Bo-Gong <3614204641@qq.com>
2026-08-11 19:57:13 +0800

    尾递归拼接

--- bin/main.ml
+++ bin/main.ml
@@ -26,9 +26,9 @@ let () =
         let optimized_ir =
           if enable_opt then
             ir
-            (* |> Lib.Ir.arithmetic_optimize      常量折叠 + 算术优化 *)
-            (* |> Lib.Ir.common_subexpression_elimination    CSE *)
-            (* |> Lib.Ir.tail_recursion_optimize    *)
+            |> Lib.Ir.arithmetic_optimize      (*常量折叠 + 算术优化*)
+            |> Lib.Ir.common_subexpression_elimination   (* CSE*)
+            |> Lib.Ir.tail_recursion_optimize   
             (* |> Lib.Ir.register_allocation *)
             (* |> Lib.Ir.licm_optimize               *)
             (* |> Lib.Ir.dead_code_elimination     *)
