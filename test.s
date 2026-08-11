3d2c2fdf8c28fbcb3c41274c4022acf7fb4a2994
Bo-Gong <3614204641@qq.com>
2026-08-11 18:33:44 +0800

    licm回退

--- test.s
+++ test.s
@@ -1,79 +1,61 @@
     .text
 
-    .globl N
-    .data
-    .align 2
-N:
-    .word 20
-
-    .globl A
-    .data
-    .align 2
-A:
-    .word 5
+    .text
+    .globl sum
+sum:
+    addi sp, sp, -32
+    sw ra, 28(sp)
+    sw fp, 24(sp)
+    addi fp, sp, 32
+    sw a0, -12(fp)
+    sw a1, -16(fp)
+    lw t0, -12(fp)
+    li t1, 0
+    slt t0, t1, t0
+    xori t0, t0, 1
+    sw t0, -20(fp)
+    lw t0, -20(fp)
+    beqz t0, L0
+    lw a0, -16(fp)
+    j .L_epilogue_sum
+L0:
+    lw t0, -12(fp)
+    li t1, 1
+    sub t0, t0, t1
+    sw t0, -24(fp)
+    lw t0, -16(fp)
+    lw t1, -12(fp)
+    add t0, t0, t1
+    sw t0, -28(fp)
+    lw a0, -24(fp)
+    lw a1, -28(fp)
+    call sum
+    sw a0, -32(fp)
+    lw a0, -32(fp)
+    j .L_epilogue_sum
+L1:
+.L_epilogue_sum:
+    lw ra, -4(fp)
+    lw fp, -8(fp)
+    addi sp, sp, 32
+    ret
 
     .text
     .globl main
 main:
-    addi sp, sp, -48
-    sw ra, 44(sp)
-    sw fp, 40(sp)
-    addi fp, sp, 48
-    lw t0, -48(fp)
-    sw t0, -16(fp)
-    li t0, 0
-    sw t0, -20(fp)
-    li t0, 0
-    sw t0, -16(fp)
-    li t0, 10
-    sw t0, -24(fp)
-    lw t0, -24(fp)
-    sw t0, -12(fp)
-L0:
-    lw t0, -16(fp)
-    la t1, N
-    lw t1, 0(t1)
-    slt t0, t0, t1
-    sw t0, -28(fp)
-    lw t0, -28(fp)
-    beqz t0, L2
-L1:
-    lw t0, -16(fp)
-    li t1, 10
-    slt t0, t1, t0
-    sw t0, -32(fp)
-    lw t0, -32(fp)
-    beqz t0, L3
-    lw t0, -12(fp)
-    lw t1, -12(fp)
-    add t0, t0, t1
-    sw t0, -36(fp)
-    lw t0, -20(fp)
-    lw t1, -36(fp)
-    add t0, t0, t1
-    sw t0, -40(fp)
-    lw t0, -40(fp)
-    sw t0, -20(fp)
-    j L4
-L3:
-    lw t0, -20(fp)
-    lw t1, -12(fp)
-    add t0, t0, t1
-    sw t0, -44(fp)
-    lw t0, -44(fp)
-    sw t0, -20(fp)
-L4:
-    lw t0, -16(fp)
-    li t1, 1
-    add t0, t0, t1
-    sw t0, -48(fp)
-    j L0
-L2:
-    lw a0, -20(fp)
+    addi sp, sp, -16
+    sw ra, 12(sp)
+    sw fp, 8(sp)
+    addi fp, sp, 16
+    li a0, 100
+    li a1, 0
+    call sum
+    sw a0, -12(fp)
+    lw a0, -12(fp)
     j .L_epilogue_main
 .L_epilogue_main:
     lw ra, -4(fp)
     lw fp, -8(fp)
-    addi sp, sp, 48
+    addi sp, sp, 16
     ret
 
--- test/test.c
+++ test/test.c
@@ -1,19 +1,14 @@
-// test_licm_3.tc
-const int N = 20;
-const int A = 5;
+// p06_tail_recursion.tc
+// 测试尾递归
+
+int sum(int n, int acc) {
+    if (n <= 0) {
+        return acc;
+    } else {
+        return sum(n - 1, acc + n);  // 尾递归！
+    }
+}
 
 int main() {
-    int sum = 0;
-    int i = 0;
-    int step = A * 2;        // 10（不变量）
-    
-    while (i < N) {
-        if (i > 10) {
-            sum = sum + step * 2;   // step 不变
-        } else {
-            sum = sum + step;       // step 不变
-        }
-        i = i + 1;
-    }
-    return sum;              // 10*10 + 10*20 = 300
+    return sum(100, 0);  // 5050
 }
